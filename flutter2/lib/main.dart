bac2bf91bb66a5cd81d4f9cdc52325234e560384
kingcwt <kingcwt@qq.com>
2023-11-14 18:05:10 +0800

    feat: StatefulWidget 有状态组件传值

--- flutter2/lib/main.dart
+++ flutter2/lib/main.dart
@@ -1,129 +1,92 @@
-/// button 详解
+/// StatefulWidget 有状态组件传值
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Flutter Demo')),
-        body: const MyBody(),
-      )));
+      theme: ThemeData(primarySwatch: Colors.yellow), home: const HomePage()));
 }
 
-class MyBody extends StatelessWidget {
-  const MyBody({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int countNum = 0;
+  onPressed() {
+    setState(() {
+      countNum++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(10),
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            print('print 普遍按钮');
-          },
-          child: const Text('普遍按钮'),
-        ),
-        TextButton(
-            onPressed: () {
-              print('print 文本按钮');
-            },
-            child: const Text('文本按钮')),
-        OutlinedButton(
-            onPressed: () {
-              print('print 线框按钮');
-            },
-            child: const Text('线框按钮')),
-        IconButton(
-          onPressed: () {
-            print('print 图标按钮');
-          },
-          icon: const Icon(Icons.thumb_up_alt_outlined),
-        ),
-        const Divider(),
-        ElevatedButton.icon(
-            onPressed: () {
-              print('print 带图标的普通按钮');
-            },
-            icon: const Icon(Icons.send),
-            label: const Text('发送')),
-        TextButton.icon(
-            onPressed: () {
-              print('print 带图标的文本按钮');
-            },
-            icon: const Icon(Icons.thumb_up_alt_outlined),
-            label: const Text('点赞')),
-        OutlinedButton.icon(
-          onPressed: () {
-            print('print 带图标的线框按钮');
-          },
-          icon: const Icon(Icons.thumb_down),
-          label: const Text('点踩'),
-        ),
-        const Divider(),
-        ElevatedButton(
-          onPressed: () {
-            print('print 带颜色的普通按钮');
-          },
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.red), // 背景颜色
-            foregroundColor: MaterialStateProperty.all(Colors.white), // 文字颜色
-          ),
-          child: const Text('带颜色的普通按钮'),
-        ),
-        const Divider(),
-        // 设置宽高的普通按钮
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(10),
-              // height: 60,
-              width: 250,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size.fromWidth(50),
-                ),
-                onPressed: () {},
-                child: const Text('设置宽高的普通按钮'),
-              ),
-            )
-          ],
-        ),
-        // 设置圆角
-        Row(
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)))),
-              child: const Text('圆角'),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all(Colors.red),
-                  backgroundColor: MaterialStateProperty.all(Colors.white),
-                  shape: MaterialStateProperty.all(const CircleBorder(
-                      side: BorderSide(color: Colors.blue, width: 2)))),
-              child: const Text('圆角'),
-            )
-          ],
-        ),
-        Row(
-          children: [
-            OutlinedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                side: MaterialStateProperty.all(const BorderSide(
-                    color: Color.fromARGB(255, 202, 36, 24), width: 2)),
-              ),
-              child: const Text('边框'),
-            )
-          ],
-        )
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
+      ),
+      body: MyBody(countNum, onPressed),
+      floatingActionButton: FloatingActionButton(
+        onPressed: onPressed,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
+
+class MyBody extends StatefulWidget {
+  final int countNum;
+  final VoidCallback onPressed;
+  const MyBody(this.countNum, this.onPressed, {super.key});
+
+  @override
+  State<MyBody> createState() => _MyBodyState();
+}
+
+class _MyBodyState extends State<MyBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(children: [
+          Text('${widget.countNum}',
+              style: const TextStyle(
+                fontSize: 28,
+              )),
+          const SizedBox(height: 100),
+          ElevatedButton(onPressed: widget.onPressed, child: const Text('add'))
+        ])
+      ],
+    ));
+  }
+}
+// class MyBody extends StatelessWidget {
+//   int countNum = 0;
+//   MyBody({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return (Center(
+//         child: Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Column(children: [
+//           Text('$countNum',
+//               style: const TextStyle(
+//                 fontSize: 28,
+//               )),
+//           const SizedBox(height: 100),
+//           ElevatedButton(
+//               onPressed: () {
+//                 countNum++;
+//                 print(countNum);
+//               },
+//               child: const Text('add'))
+//         ])
+//       ],
+//     )));
+//   }
+// }
