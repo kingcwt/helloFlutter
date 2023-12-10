977469933203ba595c6bf2bcba0ec5708b32fe02
kingcwt <kingcwt@qq.com>
2023-12-10 18:32:50 +0800

    feat: 显示动画 ScaleTransition

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
+/// AnimatedContainer
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Myapp());
+  runApp(const MyApp());
 }
 
-class Myapp extends StatefulWidget {
-  const Myapp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  State<Myapp> createState() => _MyappState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyappState extends State<Myapp> {
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,77 +35,78 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      vsync: this, // 让程序和手机的刷新频率统一
+      duration: const Duration(seconds: 1),
+      vsync: this,
       lowerBound: 0.5,
       upperBound: 1.0,
-      duration: const Duration(seconds: 1),
-    );
+    )..repeat(reverse: true);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _controller.forward();
-          _controller.reverse();
-          _controller.repeat();
-          _controller.repeat(reverse: true);
-          _controller.reset();
-          _controller.dispose();
-        },
-        child: const Icon(Icons.add),
-      ),
-      appBar: AppBar(
-        title: const Text('Flutter Demo'),
-      ),
-      body: Center(
+        appBar: AppBar(
+          title: const Text('Flutter Demo1'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {});
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: Center(
           child: Column(children: [
-        const SizedBox(
-          height: 40,
-        ),
-        FadeTransition(
-            opacity: _controller,
-            // turns: _controller,
-            child: const FlutterLogo()),
-        const SizedBox(
-          height: 40,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  _controller.forward(); // 播放一次
-                },
-                child: const Text('forward')),
-            ElevatedButton(
-                onPressed: () {
-                  _controller.reverse(); // 倒序播放一次
-                },
-                child: const Text('reverse')),
-            ElevatedButton(
-                onPressed: () {
-                  _controller.stop(); // 停止
-                },
-                child: const Text('stop')),
-            ElevatedButton(
-                onPressed: () {
-                  _controller.reset(); //  重置
-                },
-                child: const Text('reset')),
-            ElevatedButton(
-                onPressed: () {
-                  _controller.repeat(); //  重复播放
-                },
-                child: const Text('repeat')),
-          ],
-        )
-      ])),
-    );
+            const SizedBox(
+              height: 40,
+            ),
+            ScaleTransition(
+              scale: _controller
+                  .drive(Tween(begin: 0.5, end: 1.2)), // Tween配置 初始化 从0.5 到 1.2
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.forward(); // 播放一次
+                    },
+                    child: const Text('forward')),
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.reverse(); // 倒序播放一次
+                    },
+                    child: const Text('reverse')),
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.stop(); // 停止
+                    },
+                    child: const Text('stop')),
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.reset(); //  重置
+                    },
+                    child: const Text('reset')),
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.repeat(); //  重复播放
+                    },
+                    child: const Text('repeat')),
+              ],
+            )
+          ]),
+        ));
   }
 }
---
+++ libDemo/marinReact.0_0_5.dart
@@ -0,0 +1,111 @@
+// 显示动画
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this, // 让程序和手机的刷新频率统一
+      lowerBound: 0.5,
+      upperBound: 1.0,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _controller.forward();
+          _controller.reverse();
+          _controller.repeat();
+          _controller.repeat(reverse: true);
+          _controller.reset();
+          _controller.dispose();
+        },
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+      ),
+      body: Center(
+          child: Column(children: [
+        const SizedBox(
+          height: 40,
+        ),
+        FadeTransition(
+            opacity: _controller,
+            // turns: _controller,
+            child: const FlutterLogo()),
+        const SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  _controller.forward(); // 播放一次
+                },
+                child: const Text('forward')),
+            ElevatedButton(
+                onPressed: () {
+                  _controller.reverse(); // 倒序播放一次
+                },
+                child: const Text('reverse')),
+            ElevatedButton(
+                onPressed: () {
+                  _controller.stop(); // 停止
+                },
+                child: const Text('stop')),
+            ElevatedButton(
+                onPressed: () {
+                  _controller.reset(); //  重置
+                },
+                child: const Text('reset')),
+            ElevatedButton(
+                onPressed: () {
+                  _controller.repeat(); //  重复播放
+                },
+                child: const Text('repeat')),
+          ],
+        )
+      ])),
+    );
+  }
+}
